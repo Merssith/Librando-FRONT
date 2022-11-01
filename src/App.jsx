fa1018e0105e4ac08743615977904f4283f7f2f7
julianpacheco1 <jpjulianpacheco@gmail.com>
2022-11-01 13:03:19 -0300

    probando modif

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-
+import Home from "./components/Home";
 const App = () => {
-  return <div>Hola</div>;
+  return (
+    <div>
+      <div>Hola</div>
+      <Home />
+    </div>
+  );
 };
 
 export default App;
