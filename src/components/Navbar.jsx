5208d2367c191f8382564c229034cb8280641cd2
alberto <albertokarri@gmail.com>
2022-11-03 19:14:49 -0300

    funcionalidad login y register, redux user, vistas

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,71 +1,109 @@
 import React from "react";
-
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
+import { useSelector } from "react-redux";
+import {
+  Container,
+  Navbar,
+  Nav,
+  NavDropdown,
+  Button,
+  Form,
+  Row,
+} from "react-bootstrap";
 
 const NavBar = () => {
+  const user = useSelector((state) => state.user);
+
   return (
-    <Navbar bg="light" expand="lg">
-      <Container fluid>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-
-        <Nav>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <NavDropdown
-              title="Libros"
-              id="basic-nav-dropdown"
-              className="me-2"
-            >
-              <NavDropdown.Item href="/books/accion">Accion</NavDropdown.Item>
-              <NavDropdown.Item href="/books/terror">Terror</NavDropdown.Item>
-              <NavDropdown.Item href="/books/romance">Romance</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="/books/all">Todos</NavDropdown.Item>
-            </NavDropdown>
-            <Form className="d-flex">
-              <Form.Control
-                size="sm"
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success" size="sm">
-                Search
-              </Button>
-            </Form>
-          </Navbar.Collapse>
-        </Nav>
-
-        <Nav>
-          <Navbar.Brand href="#home">LIBRANDO</Navbar.Brand>
-        </Nav>
-
-        <Nav>
-          <Nav.Link href="/checkout">
+    <Container fluid>
+      <Row>
+        <Nav className="justify-content-center bg-light">
+          <Navbar.Brand href="/home">
             <img
-              src="https://w7.pngwing.com/pngs/225/984/png-transparent-computer-icons-shopping-cart-encapsulated-postscript-shopping-cart-angle-black-shopping.png"
-              width="auto"
-              height="30"
-              className="d-inline-block align-top"
-              alt="Checkout"
+              src=""
+              alt=""
+              width="30"
+              height="24"
+              class="d-inline-block align-text-top"
             />
-          </Nav.Link>
-          <Nav.Link href="/login">
-            <Button size="sm">Acceso</Button>
-          </Nav.Link>
-          <Nav.Link href="/signup">
-            <Button variant="secondary" size="sm">
-              Registro
-            </Button>
-          </Nav.Link>
+            Librando
+          </Navbar.Brand>
         </Nav>
-      </Container>
-    </Navbar>
+      </Row>
+      <Row>
+        <Navbar bg="light" expand="sm">
+          <Container fluid>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav>
+                <NavDropdown
+                  title="Libros"
+                  id="basic-nav-dropdown"
+                  className="me-2"
+                >
+                  <NavDropdown.Item href="/books/accion">
+                    Accion
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="/books/terror">
+                    Terror
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="/books/romance">
+                    Romance
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="/books/all">Todos</NavDropdown.Item>
+                </NavDropdown>
+                <Form className="d-flex">
+                  <Form.Control
+                    size="sm"
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                  <Button variant="outline-success" size="sm">
+                    Search
+                  </Button>
+                </Form>
+              </Nav>
+            </Navbar.Collapse>
+            <Nav>
+              <Nav.Link href="/checkout">
+                <img
+                  src="https://w7.pngwing.com/pngs/225/984/png-transparent-computer-icons-shopping-cart-encapsulated-postscript-shopping-cart-angle-black-shopping.png"
+                  width="auto"
+                  height="30"
+                  className="d-inline-block align-top"
+                  alt="Checkout"
+                />
+              </Nav.Link>
+              {user.id ? (
+                <>
+                  <Nav.Link href="/user">
+                    <Button size="sm">{user.name}</Button> {/* cambiar boton */}
+                  </Nav.Link>
+                  <Nav.Link href="/logout">
+                    <Button variant="secondary" size="sm">
+                      Log out
+                    </Button>
+                  </Nav.Link>
+                </>
+              ) : (
+                <>
+                  <Nav.Link href="/login">
+                    <Button size="sm">Acceso</Button>
+                  </Nav.Link>
+                  <Nav.Link href="/signup">
+                    <Button variant="secondary" size="sm">
+                      Registro
+                    </Button>
+                  </Nav.Link>
+                </>
+              )}
+            </Nav>
+          </Container>
+        </Navbar>
+      </Row>
+    </Container>
   );
 };
 
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router";
+import { useDispatch } from "react-redux";
+import { sendLoginRequest } from "../state/user";
+import axios from "axios";
 
 import useInput from "../hooks/useInput";
+import { joinAddress } from "../utils";
 
-import Button from "react-bootstrap/Button";
-import Col from "react-bootstrap/Col";
-import Form from "react-bootstrap/Form";
-import Row from "react-bootstrap/Row";
+import { Button, Col, Row, Form } from "react-bootstrap";
 
 const Signup = () => {
   const [validated, setValidated] = useState(false);
-  const [adress, setAdress] = useState("");
+  const [address, setAddress] = useState("");
   const name = useInput();
   const lastname = useInput();
   const dni = useInput();
@@ -23,6 +24,7 @@ const Signup = () => {
   const state = useInput();
   const cpa = useInput();
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -30,126 +32,162 @@ const Signup = () => {
     if (form.checkValidity() === false) {
       e.stopPropagation();
     } else {
-      setAdress(
-        `${dir.value}, ${num.value}, ${dpto.value}, ${city.value}, ${state.value}, ${cpa.value}`
+      setAddress(
+        joinAddress([
+          dir.value,
+          num.value,
+          dpto.value,
+          city.value,
+          state.value,
+          cpa.value,
+        ])
       );
-      alert("pendiente funcionalidad" + adress);
-      navigate("/");
+      axios
+        .post("/register", {
+          name,
+          lastname,
+          email,
+          password,
+          dni,
+          address,
+        })
+        .then((res) => res.data)
+        .then(({ email, password }) => {
+          // tras el registro hacemos login automatico y redirigimos a home
+          dispatch(sendLoginRequest({ email, password }));
+          navigate("/");
+        })
+        .catch(() => alert("Failed register!"));
     }
     setValidated(true);
   };
-  return (
-    <Form noValidate validated={validated} onSubmit={handleSubmit}>
-      <Row className="mb-3">
-        <Form.Group as={Col} md="4" controlId="validationCustom01">
-          <Form.Label>Nombre</Form.Label>
-          <Form.Control required type="text" placeholder="Nombre" {...name} />
-          <Form.Control.Feedback type="invalid">
-            Por favor introduce un nombre.
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="4" controlId="validationCustom02">
-          <Form.Label>Apellido</Form.Label>
-          <Form.Control
-            required
-            type="text"
-            placeholder="Apellido"
-            {...lastname}
-          />
-          <Form.Control.Feedback type="invalid">
-            Por favor introduce un apellido.
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="4" controlId="validationCustom03">
-          <Form.Label>DNI</Form.Label>
-          <Form.Control required type="text" placeholder="Nombre" {...dni} />
-          <Form.Control.Feedback type="invalid">
-            Por favor introduce un DNI.
-          </Form.Control.Feedback>
-        </Form.Group>
-      </Row>
 
-      <Row className="mb-3">
-        <Form.Group as={Col} md="6" controlId="formBasicEmail">
-          <Form.Label>Email</Form.Label>
-          <Form.Control required type="email" placeholder="Email" {...email} />
-          <Form.Control.Feedback type="invalid">
-            Por favor introduce mail de tipo ejemplo@ejemplo.com
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="6" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
+  return (
+    <>
+      <h2 className="is-title is-size-2">Sign up for an account</h2>
+      <p className="mb-5">Fill out the form below to get started.</p>
+      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+        <Row className="mb-3">
+          <Form.Group as={Col} md="4" controlId="validationCustom01">
+            <Form.Label>Nombre</Form.Label>
+            <Form.Control required type="text" placeholder="Nombre" {...name} />
+            <Form.Control.Feedback type="invalid">
+              Por favor introduce un nombre.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="4" controlId="validationCustom02">
+            <Form.Label>Apellido</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              placeholder="Apellido"
+              {...lastname}
+            />
+            <Form.Control.Feedback type="invalid">
+              Por favor introduce un apellido.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="4" controlId="validationCustom03">
+            <Form.Label>DNI</Form.Label>
+            <Form.Control required type="text" placeholder="Nombre" {...dni} />
+            <Form.Control.Feedback type="invalid">
+              Por favor introduce un DNI.
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Row>
+
+        <Row className="mb-3">
+          <Form.Group as={Col} md="6" controlId="formBasicEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              required
+              type="email"
+              placeholder="Email"
+              {...email}
+            />
+            <Form.Control.Feedback type="invalid">
+              Por favor introduce mail ejemplo@ejemplo.com
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="6" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              required
+              type="password"
+              placeholder="Password"
+              {...password}
+            />
+            <Form.Control.Feedback type="invalid">
+              Por favor introduce una contraseña.
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Row>
+        <Row className="mb-3">
+          <Form.Group as={Col} md="8" controlId="validationCustom04">
+            <Form.Label>Direccion</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Direccion"
+              required
+              {...dir}
+            />
+            <Form.Control.Feedback type="invalid">
+              Por favor introduce una direccion.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="2" controlId="validationCustom05">
+            <Form.Label>Num.</Form.Label>
+            <Form.Control type="text" placeholder="Num." required {...num} />
+            <Form.Control.Feedback type="invalid">
+              Por favor introduce un numero.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="2" controlId="validationCustom06">
+            <Form.Label>Dpto.</Form.Label>
+            <Form.Control type="text" placeholder="Dpto." {...dpto} />
+          </Form.Group>
+        </Row>
+        <Row className="mb-3">
+          <Form.Group as={Col} md="6" controlId="validationCustom07">
+            <Form.Label>Ciudad</Form.Label>
+            <Form.Control type="text" placeholder="Ciudad" required {...city} />
+            <Form.Control.Feedback type="invalid">
+              Por favor introduce una ciudad.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="3" controlId="validationCustom08">
+            <Form.Label>Provincia</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Provincia"
+              required
+              {...state}
+            />
+            <Form.Control.Feedback type="invalid">
+              Por favor introduce una provincia.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md="3" controlId="validationCustom09">
+            <Form.Label>CPA</Form.Label>
+            <Form.Control type="text" placeholder="CPA" required {...cpa} />
+            <Form.Control.Feedback type="invalid">
+              Por favor introduce un codigo postal.
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Row>
+        <Form.Group className="mb-3">
+          <Form.Check
             required
-            type="password"
-            placeholder="Password"
-            {...password}
+            label="Acepto términos y condiciones"
+            feedback="Debes aceptar para poder registrarte."
+            feedbackType="invalid"
           />
-          <Form.Control.Feedback type="invalid">
-            Por favor introduce una contraseña.
-          </Form.Control.Feedback>
         </Form.Group>
-      </Row>
-      <Row className="mb-3">
-        <Form.Group as={Col} md="8" controlId="validationCustom04">
-          <Form.Label>Direccion</Form.Label>
-          <Form.Control type="text" placeholder="Direccion" required {...dir} />
-          <Form.Control.Feedback type="invalid">
-            Por favor introduce una direccion.
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="2" controlId="validationCustom05">
-          <Form.Label>Num.</Form.Label>
-          <Form.Control type="text" placeholder="Num." required {...num} />
-          <Form.Control.Feedback type="invalid">
-            Por favor introduce un numero.
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="2" controlId="validationCustom06">
-          <Form.Label>Dpto.</Form.Label>
-          <Form.Control type="text" placeholder="Dpto." {...dpto} />
-        </Form.Group>
-      </Row>
-      <Row className="mb-3">
-        <Form.Group as={Col} md="6" controlId="validationCustom07">
-          <Form.Label>Ciudad</Form.Label>
-          <Form.Control type="text" placeholder="Ciudad" required {...city} />
-          <Form.Control.Feedback type="invalid">
-            Por favor introduce una ciudad.
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="3" controlId="validationCustom08">
-          <Form.Label>Provincia</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Provincia"
-            required
-            {...state}
-          />
-          <Form.Control.Feedback type="invalid">
-            Por favor introduce una provincia.
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="3" controlId="validationCustom09">
-          <Form.Label>CPA</Form.Label>
-          <Form.Control type="text" placeholder="CPA" required {...cpa} />
-          <Form.Control.Feedback type="invalid">
-            Por favor introduce un codigo postal.
-          </Form.Control.Feedback>
-        </Form.Group>
-      </Row>
-      <Form.Group className="mb-3">
-        <Form.Check
-          required
-          label="Acepto términos y condiciones"
-          feedback="Debes aceptar para poder registrarte."
-          feedbackType="invalid"
-        />
-      </Form.Group>
-      <Button variant="primary" size="lg" type="submit">
-        Registro
-      </Button>
-    </Form>
+        <Button variant="primary" type="submit">
+          Registro
+        </Button>
+      </Form>
+    </>
   );
 };
 
