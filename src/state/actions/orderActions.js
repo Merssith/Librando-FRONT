db86a396cbded110e058f283fc922e4c0ab041f3
francolivelli <franco.livelli@gmail.com>
2022-11-11 15:15:50 -0300

    fixes

--- src/state/actions/orderActions.js
+++ src/state/actions/orderActions.js
@@ -1,17 +1,20 @@
-import { SAVE_SHIPPING_ADDRESS, SAVE_PAYMENT_METHOD } from "../constants/orderConstants";
+import {
+  SAVE_SHIPPING_ADDRESS,
+  SAVE_PAYMENT_METHOD,
+} from "../constants/orderConstants";
 
-export const saveShippingAddress = data => async dispatch => {
-    dispatch({
-        type: SAVE_SHIPPING_ADDRESS,
-        payload: data,
-    })
-    localStorage.setItem("shippingAddress", JSON.stringify(data))
-}
+export const saveShippingAddress = (data) => async (dispatch) => {
+  dispatch({
+    type: SAVE_SHIPPING_ADDRESS,
+    payload: data,
+  });
+  localStorage.setItem("shippingAddress", JSON.stringify(data));
+};
 
-export const savePaymentMethod = data => async dispatch => {
-    dispatch({
-        type: SAVE_PAYMENT_METHOD,
-        payload: data,
-    })
-    localStorage.setItem("paymentMethod", JSON.stringify(data))
-}
+export const savePaymentMethod = (data) => async (dispatch) => {
+  dispatch({
+    type: SAVE_PAYMENT_METHOD,
+    payload: data,
+  });
+  localStorage.setItem("paymentMethod", JSON.stringify(data));
+};
